8da03b9ee22d0f7e88024f26283a133ccae6e256
Vladimir <32768348+Fronun@users.noreply.github.com>
2018-11-14 16:45:47 +0500

    адын!!! адын!!! НЕПРАВИЛЬНО БЛЯТЬ

--- Localizations/english.lang.lua
+++
@@ -1,8 +0,0 @@
-{
-download = "Download",
-alert0 = "Do not remove the internet map during downloads, it will take 3 seconds.",
-alert1 = "File Loading is successfully! To put on your desktop, Settings -> wallpapers, choose: ",
-lan = "Language",
-author = "Author",
-author1 = "Author: Fronun. (Helped: MineCR, ECS) Github: https://github.com/Fronun"
-}
